6455b08618342c5481266e16170f1b8540d1354e
ChoeYeongho <rhkeh102416@gmail.com>
2024-05-05 22:56:00 +0900

    test : LoginFragment -> firestore에 접근을 테스트 하기 위한 함수들이 추가되었습니다. userTableCheck, userTableCheckQuery, createUserTable,userMeetingGetQuery 같은 테스트 함수들이 추가 되었습니다. MainActivityViewModel -> UID를 계속 가지고 있을 수 있도록 MainActivityViewModel 파일을 추가 하였습니다.
    (파이어 베이스 오류에 따른 "default_web_client_id"가 빨간줄이 생겨있습니다. 작동에는 아무런 이상이 없습니다.)

--- app/src/main/java/com/ping/app/presentation/ui/feature/login/LoginFragment.kt
+++ app/src/main/java/com/ping/app/presentation/ui/feature/login/LoginFragment.kt
@@ -1,29 +1,32 @@
 package com.ping.app.presentation.ui.feature.login
 
 import android.content.Intent
+import android.os.Build.VERSION_CODES.P
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.databinding.adapters.TextViewBindingAdapter.setText
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.ping.app.R
 import com.ping.app.databinding.FragmentLoginBinding
 import com.ping.app.presentation.base.BaseFragment
+import com.ping.app.presentation.ui.container.MainActivityViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.UUID
 
 
 private const val TAG = "LoginFragment_싸피"
@@ -71,6 +74,9 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>(R.layou
 
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+    private val mainActivityViewModel:MainActivityViewModel by viewModels()
+    private val db = Firebase.firestore
+
 
     /**
      * 로그인 프래그먼트의 시작 부분입니다.
@@ -87,6 +93,9 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>(R.layou
                 signIn()
             }
         }
+
+        Log.d(TAG, "initView@@@@@@@viewmodel: ${mainActivityViewModel.userUid.value}")
+
     }
 
     /**
@@ -113,6 +122,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>(R.layou
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         updateUI(currentUser)
+        getUserUid(currentUser)
     }
 
 //     [START onactivityresult]
@@ -167,10 +177,156 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>(R.layou
         if (user != null) {
 
             binding.loginTv.setText(user.displayName.toString())
-
+            userTableCheck(user.uid)
         } else {
             binding.loginTv.setText("인증 실패")
         }
+    }
+
+    /**
+     * 해당 함수는 유저의 Uid를 MainActivity의 Viewmodel에 저장하는 기능을 가진 함수입니다.
+     * mainViewmodel에 넣은 이유는 user의 uid가 메인 액티비티가 살아있는 동안은 계속
+     * 유지가 되어야 한다고 생각을 했기 때문입니다.
+     *
+     * 추후 본 함수는 다른 파일로 분리될 예정입니다.
+     */
+    fun getUserUid(user: FirebaseUser?){
+        if (user != null) {
+            mainActivityViewModel.saveUserUid(user.uid)
+        }
+    }
+
+    /**
+     * 하단의 함수는 유저의 테이블 생성 유무를 체크하고 만일 테이블이 없다면 생성하는 함수를 호출할 것이며
+     * 이미 테이블이 존재 한다면 해당 테이블을 불러 올 것입니다.
+     *
+     * 추후 본 함수는 다른 파일로 분리될 예정입니다.
+     */
+    fun userTableCheck(UID : String){
+//       테스트 용
+//        lifecycleScope.launch {
+//            createUserTable("1")
+//        }
+
+        lifecycleScope.launch {
+            if (userTableCheckQuery(UID) == true) { // 해당 UID를 가지는 유저 테이블이 있을 경우
+                // 테이블이 있다면 메인 프래그먼트로 넘어 감
+                Log.d(TAG, "userTableCheck: user table mounted")
+
+                // 테스트 용
+//                userMeetingGetQuery(UID)
+
+
+            } else { // 해당 UID를 가지는 유저 테이블이 없는 경우
+                // 없을 경우 해당 테이블을 만들어야 함
+//                lifecycleScope.launch {
+//                    when (createUserTable(UID)) {
+//                        true -> { // 유저 테이블 생성 완료
+//
+//                        }
+//
+//                        false -> { // 유저 테이블 생성 실패
+//
+//                        }
+//
+//                        else -> { // 유저 테이블 생성 실패
+//
+//                        }
+//                    }
+//                }
+            }
+        }
+    }
+
+
+
+
+    /**
+     * 아래의 함수는 Firestore에 접근하여 값을 리턴 받는 함수 입니다.
+     *
+     * return -> true
+     * UID에 해당하는 유저 테이블이 존재할 경우에는 true를 반환합니다.
+     *
+     * return -> false
+     * UID에 해당하는 유저 테이블이 없을 경우와 디비 접근에 실패 했을 경우에는 false를 반환합니다.
+     *
+     * 추후 본 함수는 다른 파일로 분리될 예정입니다.
+     */
+    suspend fun userTableCheckQuery(UID : String) : Boolean{
+        var result = false
+        val docRef = db.collection("USER").document(UID)
+        var afdocRef: Task<DocumentSnapshot>? = null
+
+        lifecycleScope.launch {
+            CoroutineScope(Dispatchers.IO).launch{
+                afdocRef = docRef.get()
+            }.join()
+
+            if (afdocRef != null) {
+                Log.d(TAG, "DocumentSnapshot data:${
+                    afdocRef!!.addOnSuccessListener {
+                        Log.d(TAG, "userTableCheckQuery: ${it.data}")
+                }} ")
+                result = true
+            } else {
+                Log.d(TAG, "No such document")
+            }
+        }.join()
+
+        Log.d(TAG, "userTableCheckQuery1 ${result}")
+        return result
+    }
+
+
+    /**
+     * 아래의 함수는 만일 유저 테이블이 없을 경우 유저 테이블을 생성하는 함수 입니다.
+     * 추후 UID 뿐만 아니라 USER 정보도 같이 input으로 들어옵니다.
+     *
+     * 추후 본 함수는 다른 파일로 분리될 예정입니다.
+     */
+    suspend private fun createUserTable(UID : String) : Boolean {
+        var result = false
+
+
+        val testUser: MutableMap<String, Any> = HashMap()
+        testUser["NAME"] = "test1"
+        testUser["EMAIL"] = "test@"
+        testUser["REGION"] = "USA"
+        testUser["MeetingManagerUID"] = UUID.randomUUID().mostSignificantBits.toString()
+
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                db.collection("USER").document(UID)
+                    .set(testUser)
+                    .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+                    .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+                result = true
+            }
+        }.join()
+
+        Log.d(TAG, "createUserTable: ${result}")
+        return result
+    }
+
+    /**
+     * 본 함수는 USER UID로 Meeting Table의 정보를 가져 올 수 있는지 확인 하는 함수입니다.
+     *
+     * 추후 본 함수는 다른 파일로 분리될 예정입니다.
+     */
+    fun userMeetingGetQuery(UID : String){
+
+        val docRef = db.collection("MEETING").document(UID)
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d(TAG, "DocumentSnapshot data: ${document}")
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
     }
 
     companion object {
--- app/src/main/java/com/ping/app/presentation/ui/feature/login/LoginViewModel.kt
+++ app/src/main/java/com/ping/app/presentation/ui/feature/login/LoginViewModel.kt
@@ -1,5 +1,7 @@
 package com.ping.app.presentation.ui.feature.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel:ViewModel() {
