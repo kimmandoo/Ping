2960316d6a40d6a5d12daf5fb4af4662ffc02242
kimmandoo <mingyu5675@gmail.com>
2024-05-21 14:54:59 +0900

    feat: bottom sheet fragment에 soft keyboard 적용

--- app/src/main/java/com/ping/app/ui/base/BaseBottomSheetDialogFragment.kt
+++ app/src/main/java/com/ping/app/ui/base/BaseBottomSheetDialogFragment.kt
@@ -1,10 +1,15 @@
 package com.ping.app.ui.base
 
 import android.app.Dialog
+import android.graphics.Rect
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -13,14 +18,14 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-abstract class BaseBottomSheetDialogFragment<B : ViewDataBinding, VM: ViewModel>(
+abstract class BaseBottomSheetDialogFragment<B : ViewDataBinding, VM : ViewModel>(
     @LayoutRes private val layoutId: Int,
 ) : BottomSheetDialogFragment() {
     private var _binding: B? = null
     protected val binding get() = _binding!!
     protected abstract val viewModel: VM
-
-
+    
+    
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,7 +34,7 @@ abstract class BaseBottomSheetDialogFragment<B : ViewDataBinding, VM: ViewModel>
         _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
         return binding.root
     }
-
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
@@ -37,8 +42,8 @@ abstract class BaseBottomSheetDialogFragment<B : ViewDataBinding, VM: ViewModel>
     }
     
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        // used to show the bottom sheet dialog
-        dialog?.setOnShowListener { it ->
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        dialog.setOnShowListener { it ->
             val d = it as BottomSheetDialog
             val bottomSheet =
                 d.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
@@ -47,11 +52,25 @@ abstract class BaseBottomSheetDialogFragment<B : ViewDataBinding, VM: ViewModel>
                 behavior.state = BottomSheetBehavior.STATE_EXPANDED
             }
         }
-        return super.onCreateDialog(savedInstanceState)
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            dialog.window?.setDecorFitsSystemWindows(false)
+            view?.rootView?.setOnApplyWindowInsetsListener { _, insets ->
+                val topInset = insets.getInsets(WindowInsets.Type.statusBars()).top
+                val imeHeight = insets.getInsets(WindowInsets.Type.ime()).bottom
+                val navigationHeight = insets.getInsets(WindowInsets.Type.navigationBars()).bottom
+                val bottomInset = if (imeHeight == 0) navigationHeight else imeHeight
+                view?.rootView?.setPadding(0, topInset, 0, bottomInset)
+                insets
+            }
+        } else {
+            dialog.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        }
+        return dialog
     }
-
+    
     protected abstract fun initView(savedInstanceState: Bundle?)
-
+    
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
